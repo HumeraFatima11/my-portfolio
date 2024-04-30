9cff322d5117fbd94f9a7290acc67899415ddd19
HumeraFatima11 <masoodkaleem999@gmail.com>
2024-04-30 12:46:11 +0530

    adding in sanity studio

---
+++ my-portfolio/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '4jcjr9gm',
+    dataset: 'production'
+  }
+})
---
+++ src/client.js
@@ -0,0 +1,6 @@
+import { SanityClient } from "@sanity/client";
+
+export default SanityClient({
+    projectId :"4jcjr9gm",
+    dataset : "production"
+})
